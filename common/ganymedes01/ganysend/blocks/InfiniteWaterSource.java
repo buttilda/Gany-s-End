e5792b3b0cce4ceba70c3741a0bd3cd5848c1cc8
ganymedes01 <Andre@pc-193-218.customer.stchris.uod.lan>
2013-09-29 01:36:35 +0100

    Fix for infinite water source block

--- common/ganymedes01/ganysend/blocks/InfiniteWaterSource.java
+++ common/ganymedes01/ganysend/blocks/InfiniteWaterSource.java
@@ -4,6 +4,7 @@ import ganymedes01.ganysend.GanysEnd;
 import ganymedes01.ganysend.core.utils.Utils;
 import ganymedes01.ganysend.lib.Strings;
 import ganymedes01.ganysend.tileentities.TileEntitySolidWaterSource;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -27,7 +29,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class InfiniteWaterSource extends BlockContainer {
 
 	protected InfiniteWaterSource(int id) {
-		super(id, Material.water);
+		super(id, Material.rock);
 		setHardness(3.5F);
 		setCreativeTab(GanysEnd.endTab);
 		setUnlocalizedName(Utils.getUnlocalizedName(Strings.INFINITE_WATER_SOURCE_NAME));
@@ -70,5 +72,37 @@ public class InfiniteWaterSource extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World world) {
 		return new TileEntitySolidWaterSource();
+	}
+
+	@Override
+	public void onBlockAdded(World world, int x, int y, int z) {
+		checkForHarden(world, x, y, z);
+	}
+
+	@Override
+	public void onNeighborBlockChange(World world, int x, int y, int z, int neighbourID) {
+		checkForHarden(world, x, y, z);
+	}
+
+	private void checkForHarden(World world, int X, int Y, int Z) {
+		for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+			int x = X + dir.offsetX;
+			int y = Y + dir.offsetY;
+			int z = Z + dir.offsetZ;
+			if (world.getBlockMaterial(x, y, z) == Material.lava) {
+				int meta = world.getBlockMetadata(x, y, z);
+				if (meta == 0)
+					world.setBlock(x, y, z, Block.obsidian.blockID);
+				else if (meta <= 4)
+					world.setBlock(x, y, z, Block.cobblestone.blockID);
+				triggerLavaMixEffects(world, x, y, z);
+			}
+		}
+	}
+
+	protected void triggerLavaMixEffects(World world, int x, int y, int z) {
+		world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+		for (int i = 0; i < 8; i++)
+			world.spawnParticle("largesmoke", x + Math.random(), y + 1.2D, z + Math.random(), 0.0D, 0.0D, 0.0D);
 	}
 }
