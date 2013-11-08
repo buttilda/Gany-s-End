53e0889544c412feac1b858ff3a648307d5d79f9
ganymedes02 <Andre@andre.home>
2013-11-08 03:33:34 +0000

    Entity Shifter now can teleport minecarts

--- common/ganymedes01/ganysend/tileentities/TileEntityEntityShifter.java
+++ common/ganymedes01/ganysend/tileentities/TileEntityEntityShifter.java
@@ -6,10 +6,16 @@ import java.util.List;
 import net.minecraft.command.IEntitySelector;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityMinecartMobSpawner;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityMinecart;
+import net.minecraft.entity.item.EntityMinecartChest;
+import net.minecraft.entity.item.EntityMinecartContainer;
+import net.minecraft.entity.item.EntityMinecartFurnace;
+import net.minecraft.entity.item.EntityMinecartHopper;
+import net.minecraft.entity.item.EntityMinecartTNT;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.AxisAlignedBB;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 
 /**
  * Gany's End
@@ -60,24 +66,66 @@ public class TileEntityEntityShifter extends TileEntityBlockShifter {
 					Entity entity = (Entity) iterator.next();
 					if (entity.worldObj == worldObj)
 						if (entity instanceof EntityLivingBase) {
-							EnderTeleportEvent event = new EnderTeleportEvent((EntityLivingBase) entity, recX + 0.5D, recY, recZ + 0.5D, 0.0F);
-							if (!MinecraftForge.EVENT_BUS.post(event)) {
-								if (entity.isRiding())
-									entity.mountEntity((Entity) null);
-								((EntityLivingBase) entity).setPositionAndUpdate(event.targetX, event.targetY, event.targetZ);
-								entity.fallDistance = 0.0F;
-								worldObj.playSoundEffect(telX, telY, telZ, "mob.endermen.portal", 1.0F, 1.0F);
-								return;
-							}
-						} else if (entity instanceof EntityItem) {
-							EntityItem newEntity = new EntityItem(worldObj, recX + 0.5D, recY, recZ + 0.5D, ((EntityItem) entity).getEntityItem());
-							worldObj.spawnEntityInWorld(newEntity);
-							entity.setDead();
+							teleportEntityLiving((EntityLivingBase) entity, recX, recY, recZ);
 							worldObj.playSoundEffect(telX, telY, telZ, "mob.endermen.portal", 1.0F, 1.0F);
-							return;
+						} else if (entity instanceof EntityItem) {
+							teleportEntityItem((EntityItem) entity, recX, recY, recZ);
+							worldObj.playSoundEffect(telX, telY, telZ, "mob.endermen.portal", 1.0F, 1.0F);
+						} else if (entity instanceof EntityMinecart) {
+							teleportEntityMinecart((EntityMinecart) entity, recX, recY, recZ);
+							worldObj.playSoundEffect(telX, telY, telZ, "mob.endermen.portal", 1.0F, 1.0F);
 						}
 				}
 			}
 		}
+	}
+
+	private void teleportEntityLiving(EntityLivingBase living, int recX, int recY, int recZ) {
+		if (living.isRiding())
+			living.mountEntity((Entity) null);
+		living.setPositionAndUpdate(recX + 0.5D, recY, recZ + 0.5D);
+		living.fallDistance = 0.0F;
+	}
+
+	private void teleportEntityItem(EntityItem item, int recX, int recY, int recZ) {
+		EntityItem newEntity = new EntityItem(worldObj, recX + 0.5D, recY, recZ + 0.5D, item.getEntityItem());
+		worldObj.spawnEntityInWorld(newEntity);
+		item.setDead();
+	}
+
+	private void teleportEntityMinecart(EntityMinecart minecart, int recX, int recY, int recZ) {
+		NBTTagCompound data = new NBTTagCompound();
+		minecart.writeToNBT(data);
+		EntityMinecart newMinecart = EntityMinecart.createMinecart(worldObj, recX, recY, recZ, getMinecartType(minecart));
+		newMinecart.readFromNBT(data);
+		newMinecart.setPosition(recX, recY, recZ);
+		worldObj.spawnEntityInWorld(newMinecart);
+
+		if (minecart instanceof EntityMinecartContainer)
+			emptyMinecartContainer((EntityMinecartContainer) minecart);
+		if (minecart.riddenByEntity != null && minecart.riddenByEntity instanceof EntityLivingBase)
+			teleportEntityLiving((EntityLivingBase) minecart.riddenByEntity, recX, recY, recZ);
+
+		minecart.setDead();
+	}
+
+	private int getMinecartType(Entity minecart) {
+		if (minecart instanceof EntityMinecartChest)
+			return 1;
+		else if (minecart instanceof EntityMinecartFurnace)
+			return 2;
+		else if (minecart instanceof EntityMinecartTNT)
+			return 3;
+		else if (minecart instanceof EntityMinecartMobSpawner)
+			return 4;
+		else if (minecart instanceof EntityMinecartHopper)
+			return 5;
+		else
+			return 0;
+	}
+
+	private void emptyMinecartContainer(EntityMinecartContainer minecart) {
+		for (int i = 0; i < minecart.getSizeInventory(); i++)
+			minecart.setInventorySlotContents(i, null);
 	}
 }
