413b6819ce59d045111b3743d1fc2dbef5493e8e
ganymedes01 <foka_12@hotmail.com>
2014-01-25 03:09:04 +0000

    Update EE3 add-on

--- common/ganymedes01/ganysend/integration/EE3Manager.java
+++ common/ganymedes01/ganysend/integration/EE3Manager.java
@@ -5,11 +5,9 @@ import ganymedes01.ganysend.items.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.oredict.OreDictionary;
-
-import com.pahimar.ee3.addon.AddonHandler;
-import com.pahimar.ee3.api.OreStack;
-import com.pahimar.ee3.emc.EmcValue;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
 /**
  * Gany's End
@@ -25,28 +23,46 @@ public class EE3Manager extends Integration {
 		addEMCValueToBlock(ModBlocks.enderFlower, 128);
 		addEMCValueToItem(ModItems.endiumIngot, 2052);
 		addEMCValueToItem(ModItems.infiniteBucket, 8196);
-		addEMCValueToStack(new ItemStack(ModItems.itemNewSkull, 1, OreDictionary.WILDCARD_VALUE), 2048);
-		addEMCValueToStack(new ItemStack(ModItems.itemNewSkull, 1, 17), 49152);
+		addEMCValue(new ItemStack(ModItems.itemNewSkull, 1, OreDictionary.WILDCARD_VALUE), 2048);
+		addEMCValue(new ItemStack(ModItems.itemNewSkull, 1, 17), 49152);
 	}
 
 	private void addEMCValueToBlock(Block block, float value) {
-		addEMCValueToStack(new ItemStack(block), value);
+		addEMCValue(new ItemStack(block), value);
 	}
 
 	private void addEMCValueToItem(Item item, float value) {
-		addEMCValueToStack(new ItemStack(item), value);
+		addEMCValue(new ItemStack(item), value);
 	}
 
-	private void addEMCValueToStack(ItemStack stack, float value) {
-		AddonHandler.sendPreValueAssignment(stack, new EmcValue(value));
+	private void addEMCValue(Object obj, float value) {
+		NBTTagCompound data = new NBTTagCompound();
+
+		data.setFloat("emcValue", value);
+
+		if (obj instanceof ItemStack) {
+			NBTTagCompound stackCompound = new NBTTagCompound();
+			((ItemStack) obj).writeToNBT(stackCompound);
+			data.setCompoundTag("itemStack", stackCompound);
+		} else if (obj instanceof String)
+			data.setString("oreName", (String) obj);
+
+		FMLInterModComms.sendMessage(getModID(), "emc-assign-value-pre", data);
 	}
 
-	private void addEMCValueToOre(String ore, float value) {
-		AddonHandler.sendPreValueAssignment(new OreStack(ore), new EmcValue(value));
-	}
+	private void addPostEMCValue(Object obj, float value) {
+		NBTTagCompound data = new NBTTagCompound();
 
-	private void addPostEMCValueToStack(ItemStack stack, float value) {
-		AddonHandler.sendPostValueAssignment(stack, new EmcValue(value));
+		data.setFloat("emcValue", value);
+
+		if (obj instanceof ItemStack) {
+			NBTTagCompound stackCompound = new NBTTagCompound();
+			((ItemStack) obj).writeToNBT(stackCompound);
+			data.setCompoundTag("itemStack", stackCompound);
+		} else if (obj instanceof String)
+			data.setString("oreName", (String) obj);
+
+		FMLInterModComms.sendMessage(getModID(), "emc-assign-value-post", data);
 	}
 
 	@Override
