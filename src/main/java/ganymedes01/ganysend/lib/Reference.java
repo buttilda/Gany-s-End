24da2d638d93e5ec52ce9534685b32f75714e478
ganymedes01 <foka_12@hotmail.com>
2014-09-07 11:48:05 +0100

    Bump version for release

--- src/main/java/ganymedes01/ganysend/lib/Reference.java
+++ src/main/java/ganymedes01/ganysend/lib/Reference.java
@@ -11,11 +11,11 @@ public class Reference {
 
 	public static final String MOD_ID = "ganysend";
 	public static final String MOD_NAME = "Gany's End";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1180,);after:ThermalExpansion;after:TwilightForest;after:EnderStorage";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1180,);after:EnderStorage";
 	public static final String CHANNEL = "GanysEnd";
 	public static final String MASTER = "GanysMods";
-	public static final String VERSION_NUMBER = "1.8.0";
-	public static final int RAW_VERSION_NUMBER = 180;
+	public static final String VERSION_NUMBER = "1.8.1";
+	public static final int RAW_VERSION_NUMBER = 181;
 	public static String LATEST_VERSION;
 	public static final String ITEM_BLOCK_TEXTURE_PATH = MOD_ID + ":";
 	public static final String ARMOUR_TEXTURE_PATH = ITEM_BLOCK_TEXTURE_PATH + "textures/models/armor/";
