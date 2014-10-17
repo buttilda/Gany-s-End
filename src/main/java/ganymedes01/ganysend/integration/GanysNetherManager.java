fe215efe8c6481519717382ce2b863205dabc4e9
ganymedes01 <foka_12@hotmail.com>
2014-10-17 17:31:01 -0300

    AOBD now adds compat for Endium automatically

--- src/main/java/ganymedes01/ganysend/integration/GanysNetherManager.java
+++ src/main/java/ganymedes01/ganysend/integration/GanysNetherManager.java
@@ -1,11 +1,8 @@
 package ganymedes01.ganysend.integration;
 
-import ganymedes01.ganysend.ModBlocks;
-import ganymedes01.ganysend.ModItems;
 import ganymedes01.ganysend.core.utils.HeadsHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import cpw.mods.fml.common.event.FMLInterModComms;
 
 /**
@@ -19,8 +16,6 @@ public class GanysNetherManager extends Integration {
 
 	@Override
 	public void init() {
-		addMagmaticCentrifugeRecipe(new ItemStack(ModBlocks.rawEndium), new ItemStack(ModBlocks.rawEndium), new ItemStack(ModItems.endiumIngot, 3), new ItemStack(ModItems.endiumIngot, 1, 1));
-
 		addStackToUndertakers(HeadsHelper.createHeadFor("ganymedes01"), 50);
 	}
 
@@ -31,40 +26,6 @@ public class GanysNetherManager extends Integration {
 	@Override
 	public String getModID() {
 		return "ganysnether";
-	}
-
-	private void addMagmaticCentrifugeRecipe(ItemStack material1, ItemStack material2, ItemStack... result) {
-		if (result.length > 4 || material1 == null || material2 == null)
-			return;
-		else
-			for (ItemStack res : result)
-				if (res == null)
-					return;
-
-		NBTTagCompound data = new NBTTagCompound();
-		NBTTagList tagList = new NBTTagList();
-		NBTTagCompound tagCompound;
-
-		tagCompound = new NBTTagCompound();
-		tagCompound.setByte("material1", (byte) 0);
-		material1.writeToNBT(tagCompound);
-		tagList.appendTag(tagCompound);
-
-		tagCompound = new NBTTagCompound();
-		tagCompound.setByte("material2", (byte) 1);
-		material2.writeToNBT(tagCompound);
-		tagList.appendTag(tagCompound);
-
-		for (int i = 0; i < result.length; i++)
-			if (result[i] != null) {
-				tagCompound = new NBTTagCompound();
-				tagCompound.setByte("result", (byte) (i + 2));
-				result[i].writeToNBT(tagCompound);
-				tagList.appendTag(tagCompound);
-			}
-		data.setTag("Recipe", tagList);
-
-		FMLInterModComms.sendMessage("ganysnether", "addCentrifugeRecipe", data);
 	}
 
 	private void addStackToUndertakers(ItemStack stack, int weight) {
