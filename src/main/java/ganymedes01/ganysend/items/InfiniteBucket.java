86d5b9a3ae569aa9fdf84d72070e62916fb43857
ganymedes01 <foka_12@hotmail.com>
2015-05-05 12:51:07 -0300

    Infinite Buckets now fill up cauldrons

--- src/main/java/ganymedes01/ganysend/items/InfiniteBucket.java
+++ src/main/java/ganymedes01/ganysend/items/InfiniteBucket.java
@@ -5,6 +5,7 @@ import ganymedes01.ganysend.IConfigurable;
 import ganymedes01.ganysend.core.utils.Utils;
 import ganymedes01.ganysend.lib.Strings;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockCauldron;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -55,6 +56,13 @@ public class InfiniteBucket extends ItemSimpleFoiled implements IConfigurable {
 			if (!world.isRemote)
 				tile.fill(dir, new FluidStack(FluidRegistry.WATER, FluidContainerRegistry.BUCKET_VOLUME), true);
 			return true;
+		} else if (world.getBlock(x, y, z) == Blocks.cauldron) {
+			int filled = BlockCauldron.func_150027_b(world.getBlockMetadata(x, y, z));
+			if (filled < 3) {
+				world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+				world.func_147453_f(x, y, z, Blocks.cauldron);
+				return true;
+			}
 		}
 
 		Block block = world.getBlock(x, y, z);
